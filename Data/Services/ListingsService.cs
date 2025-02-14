96445e08a8dcb77003e8867366b5a506d9ca1314
robertgruevski <robertgruevski@outlook.com>
2025-02-14 14:11:15 -0500

    Index page & Create Page and File Uploads

---
+++ Data/Services/ListingsService.cs
@@ -0,0 +1,26 @@
+﻿using Auctions.Data;
+using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data.Services
+{
+	public class ListingsService : IListingsService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ListingsService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task Add(Listing listing)
+		{
+			_context.Listings.Add(listing);
+			await _context.SaveChangesAsync();
+		}
+		public IQueryable<Listing> GetAll()
+		{
+			var applicationDbContext = _context.Listings.Include(l => l.User);
+			return applicationDbContext;
+		}
+	}
+}
